d811e405dad6e2d5fb6c430b8a15e511708954d9
Bianca Soares <119140865+biancasuarz@users.noreply.github.com>
2023-02-27 22:52:37 -0300

    Create jogoAdivinhacao.py

---
+++ jogoAdivinhacao.py
@@ -0,0 +1,14 @@
+import random
+import os
+erros=0
+sorteado=random.randrange(0,100)
+jogador=int(input("Digite seu número: "))
+while(sorteado!=jogador):
+    os.system('cls')
+    if(sorteado>jogador):
+        print("ERRO, o número é maior")
+    elif(sorteado<jogador):
+        print("ERRO, o número é menor")
+    erros+=1
+    jogador=int(input("Digite seu número: "))
+    print("Numero"+ str (jogador) + ", você acertou em" + str(erros+1)+ "tentativas")
